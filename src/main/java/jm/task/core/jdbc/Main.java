957afa4fc1189b2ecd20443232748098fbb6e382
mihakluch <mklucenokivse@gmail.com>
2025-03-06 00:13:14 +0300

    hibernate

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,33 +1,69 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
 import java.sql.Connection;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-//        Util.getConnection();
-        UserDao userDao = new UserDaoJDBCImpl();
+////        Util.getConnection();
+//        UserDao userDao = new UserDaoJDBCImpl();
+//
+//        userDao.createUsersTable();
+//
+//        userDao.saveUser("Михаил", "Ключенок", (byte) 43);
+//        userDao.saveUser("Мария", "Осипович", (byte) 39);
+//        userDao.saveUser("Ульяна", "Жуликовна", (byte) 12);
+//        userDao.saveUser("Дарья", "Романова", (byte) 19);
+//
+//
+////        System.out.println(userDao.getAllUsers());
+////        userDao.removeUserById(1);
+//        userDao.getAllUsers();
+//        userDao.cleanUsersTable();
+//        userDao.dropUsersTable();
 
-        userDao.createUsersTable();
+//        ---------------------------------------------------------
 
-        userDao.saveUser("Михаил", "Ключенок", (byte) 43);
-        userDao.saveUser("Мария", "Осипович", (byte) 39);
-        userDao.saveUser("Ульяна", "Жуликовна", (byte) 12);
-        userDao.saveUser("Дарья", "Романова", (byte) 19);
+        UserDao userDao = new UserDaoHibernateImpl();
 
-
-        System.out.println(userDao.getAllUsers());
-        userDao.removeUserById(1);
-        userDao.getAllUsers();
-        userDao.cleanUsersTable();
+// Удаление таблицы (если она уже есть)
         userDao.dropUsersTable();
-//            try (Connection connection = Util.getConnection()) {
-//                System.out.println("Соединение установлено!");
-//            } catch (Exception e) {
-//                System.out.println("Ошибка: " + e.getMessage());
-//            }
+        System.out.println("Таблица удалена");
+
+// Создание таблицы
+        userDao.createUsersTable();
+        System.out.println("Таблица создана");
+
+// Добавление пользователей
+            userDao.saveUser("Михаил2", "Ключенок", (byte) 43);
+       userDao.saveUser("Мария2", "Осипович", (byte) 39);
+       userDao.saveUser("Ульяна2", "Жуликовна", (byte) 12);
+        userDao.saveUser("Дарья2", "Романова", (byte) 19);
+        System.out.println("Добавлены пользователи");
+
+// Получение всех пользователей
+        List<User> users = userDao.getAllUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+// Удаление пользователя по id
+        userDao.removeUserById(1);
+        System.out.println("Удален пользователь с ID 1");
+
+// Очистка таблицы
+        userDao.cleanUsersTable();
+        System.out.println("Таблица очищена");
+
+// Удаление таблицы
+        userDao.dropUsersTable();
+        System.out.println("Таблица полностью удалена");
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,19 +1,41 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
+
     private static final String URL = "jdbc:mysql://localhost:3306/userx";
     private static final String USER = "root";
     private static final String PASSWORD = "1234554321";
 
+    private static SessionFactory sessionFactory;
+
     static {
         try {
+
             Class.forName("com.mysql.cj.jdbc.Driver");
+
+            sessionFactory = new Configuration()
+                    .setProperty("hibernate.connection.url", URL)
+                    .setProperty("hibernate.connection.username", USER)
+                    .setProperty("hibernate.connection.password", PASSWORD)
+                    .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                    .setProperty("hibernate.hbm2ddl.auto", "update")
+                    .setProperty("hibernate.show_sql", "true")
+                    .setProperty("hibernate.format_sql", "true")
+                    .setProperty("hibernate.current_session_context_class", "thread")
+                    .addAnnotatedClass(User.class)
+                    .buildSessionFactory();
+
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Ошибка загрузки драйвера!", e);
+            throw new RuntimeException("Ошибка загрузки JDBC-драйвера!", e);
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка инициализации Hibernate!", e);
         }
     }
 
@@ -21,7 +43,11 @@ public class Util {
         try {
             return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException e) {
-            throw new RuntimeException("Ошибка подключения к базе данных", e);
+            throw new RuntimeException("Ошибка подключения к базе данных!", e);
         }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
     }
 }
